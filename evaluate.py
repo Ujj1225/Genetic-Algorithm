ef4f9ac328801662985a5a55e7675412803918db
NischalPanthi <84094681+NischalPanthi@users.noreply.github.com>
2024-02-29 14:33:28 +0545

    Add files via upload

---
+++ evaluate.py
@@ -0,0 +1,38 @@
+# Take expression string, value for x, y and z as input then output result
+
+import math
+
+a = [
+    "sin",
+    "cos",
+    "tan",
+    "cosh",
+    "sinh",
+    "tanh",
+    "log",
+    "acos",
+    "asin",
+    "atan",
+    "asinh",
+    "acosh",
+    "atanh",
+    "e",
+    "pi",
+]
+
+
+def evaluate_expression(expression, x_value, y_value, z_value):
+    # Replace 'x', 'y' and 'z' with provided values in the expression
+    expression = (
+        expression.replace("x", str(x_value))
+        .replace("y", str(y_value))
+        .replace("z", str(z_value))
+    )
+
+    for i in a:
+        expression = expression.replace(i, "math." + i)
+
+    # Evaluate the expression
+    result = eval(expression)
+
+    return result
